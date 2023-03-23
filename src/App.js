d25db09ba2b168c3f6599237a2bd62eb7cde680e
aberchanedotcom <aberchanedotcom@gmail.com>
2023-03-23 11:41:36 +0000

    first commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import Navbar from './components/navbar/Navbar';
+import Formulaire from './components/form/Formulaire';
+function App() {
+  return (
+    <>
+      <Navbar/>
+      <Formulaire/>
+    </>
+  );
+}
+
+export default App;
